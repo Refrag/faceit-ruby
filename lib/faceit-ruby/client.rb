67de30117b750a464351e9ebc79f09b5ae37a780
Stefan Dorresteijn <stefan@refreshseo.nl>
2024-09-16 11:40:30 +0200

    Implement downloads API

--- lib/faceit-ruby/client.rb
+++ lib/faceit-ruby/client.rb
@@ -12,7 +12,7 @@ require "faceit-ruby/tournament"
 module Faceit
   class Client
     #init
-    API_ENDPOINT = "https://open.faceit.com/data/v4".freeze
+    API_ENDPOINT = "https://open.faceit.com/".freeze
 
     def initialize(api_key: nil)
       if api_key.nil?
@@ -36,27 +36,27 @@ module Faceit
 
     #Players
     def get_player(player_id)
-      get("players/#{player_id}", {})
+      get_data("players/#{player_id}", {})
     end
 
     def get_player_by_nickname(nickname)
-      get("players?nickname=#{nickname}", {})
+      get_data("players?nickname=#{nickname}", {})
     end
 
     def get_player_history(player_id, game_id, options = {})
-      get("players/#{player_id}/history?game=#{game_id}", options)
+      get_data("players/#{player_id}/history?game=#{game_id}", options)
     end
 
     def get_player_stats(player_id, game_id)
-      res = get("players/#{player_id}/stats/#{game_id}", {})
+      get_data("players/#{player_id}/stats/#{game_id}", {})
     end
 
     #Games
     def get_games(game_id)
       if game_id.present?
-        get("games/#{game_id}", {})
+        get_data("games/#{game_id}", {})
       else
-        res = get("games", {})
+        res = get_data("games", {})
         games = res['items'].map { |g| Game.new(g) }
         Response.new(games)
       end
@@ -64,37 +64,43 @@ module Faceit
 
     #MATCHES
     def get_match(match_id)
-      get("matches/#{match_id}", {})
+      get_data("matches/#{match_id}", {})
     end
 
     def get_match_stats(match_id)
-      get("matches/#{match_id}/stats", {})
+      get_data("matches/#{match_id}/stats", {})
     end
+
+    #DOWNLOADS
+    def get_download_url(resource_url)
+      post("download/v2/demos/download", { "resource_url": resource_url })
+    end
+
 
     #SEARCHES
     def search_organizers(options = {})
-      res = get("search/organizers", options)
+      res = get_data("search/organizers", options)
 
       organizers = res['items'].map { |g| Organizer.new(g) }
       Response.new(organizers)
     end
 
     def search_players(options = {})
-      res = get("search/players", options)
+      res = get_data("search/players", options)
 
       players = res['items'].map { |g| Player.new(g) }
       Response.new(players)
     end
 
     def search_teams(options = {})
-      res = get("search/teams", options)
+      res = get_data("search/teams", options)
 
       teams = res['items'].map { |g| Team.new(g) }
       Response.new(teams)
     end
 
     def search_tournaments(options = {})
-      res = get("search/tournaments", options)
+      res = get_data("search/tournaments", options)
 
       tournaments = res['items'].map { |g| Tournament.new(g) }
       Response.new(tournaments)
@@ -107,13 +113,29 @@ module Faceit
     end
 
 
+    def get_data(resource, params)
+      http_res = @conn.get("data/v4/#{resource}", params)
+      finish(http_res)
+    end
+
+
     def post(resource, params)
       http_res = @conn.post(resource, params)
       finish(http_res)
     end
 
+    def post_data(resource, params)
+      http_res = @conn.post("data/v4/#{resource}", params)
+      finish(http_res)
+    end
+
     def put(resource, params)
       http_res = @conn.put(resource, params)
+      finish(http_res)
+    end
+
+    def put_data(resource, params)
+      http_res = @conn.put("data/v4/#{resource}", params)
       finish(http_res)
     end
 
